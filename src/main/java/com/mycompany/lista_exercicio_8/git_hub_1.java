ac8bd3a72c123bfb44ae2f76dc0e2af4b8dab2c2
GABRIEL HENRIQUE COSTA DA SILVA <gabriel.07907@aluno.pr.senac.br>
2023-09-06 15:06:02 -0300

    add arquivo

---
+++ src/main/java/com/mycompany/lista_exercicio_8/git_hub_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista_exercicio_8;
+
+/**
+ *
+ * @author gabriel.07907
+ */
+public class git_hub_1 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
